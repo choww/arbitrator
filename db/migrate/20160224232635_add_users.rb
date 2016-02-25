5bd95f5d2d39c124fbf9e0cf4e221ce4f47b4312
Lustrado <jcarlorivera@gmail.com>
2016-02-25 17:52:14 +0000

    Initial Migration Files complete, seed data written, docs folder includes planning documents

--- db/migrate/20160224232635_add_users.rb
+++ db/migrate/20160224232635_add_users.rb
@@ -1,7 +1,10 @@
 class AddUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :username
+      t.string :email
+      t.string :password
+      t.timestamps
     end
   end
 end
