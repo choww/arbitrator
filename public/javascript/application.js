fadb06e17afa1a97cb3e475de385ae8babf66d4a
Max Lyons <danielsonlyonsmax@gmail.com>
2016-02-27 22:51:46 +0000

    added colors button clicky

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -10,6 +10,21 @@ $(document).ready(function() {
       $(thumb).closest('.question-container').find('.left-vote').text(vote[0]);
       $(thumb).closest('.question-container').find('.right-vote').text(vote[1]);
     });
+    if (value == 0) {
+      $(thumb).closest('.question-container').find('.right-button').children().addClass('red accent-2');
+      $(thumb).closest('.question-container').find('.right-button').children().removeClass('green darken-1');
+      $(thumb).closest('.question-container').find('.right-button').children().html("<i class='check material-icons'>thumb_up</i>");
+      $(thumb).html("<i class='check material-icons'>done</i>");
+      $(thumb).addClass('green darken-1');
+      $(thumb).removeClass('red accent-2');
+    } else if (value == 1) {
+      $(thumb).closest('.question-container').find('.left-button').children().addClass('red accent-2');
+      $(thumb).closest('.question-container').find('.left-button').children().removeClass('green darken-1');
+      $(thumb).closest('.question-container').find('.left-button').children().html("<i class='check material-icons'>thumb_up</i>");
+      $(thumb).html("<i class='check material-icons'>done</i>");
+      $(thumb).addClass('green darken-1');
+      $(thumb).removeClass('red accent-2');
+    }
   });
 
   $.timeago.settings.allowFuture = true;
